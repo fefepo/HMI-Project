de007723067c84816b192ff0b07b55c2ea004e32
fefepo <167264688+fefepo@users.noreply.github.com>
2026-04-10 02:06:46 +0900

    .CSV 모드/수동 모드 분리 mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -11,7 +11,7 @@
 #include "server.h"
 
 QT_USE_NAMESPACE // 네임스페이스 오류 방지
-
+    
     QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -19,11 +19,11 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    
 private slots:
     void updateSensorData();
     void on_forceXSlider_valueChanged(int value);
@@ -32,34 +32,38 @@ private slots:
     void on_accelerationSlider_valueChanged(int value);
     void on_dataReceived(QString data);
     void on_resetButton_clicked(); // 재시작 버튼
-
+    
 private:
     Ui::MainWindow *ui;
     QTimer *timer;
     QFile *logFile;
-
+    
     // CSV 읽기용 추가 변수
     QFile *inputFile = nullptr;
     QTextStream *inputStream = nullptr;
-    bool isCsvMode = true;
-
+    
+    bool manualForceX = false;
+    bool manualForceY = false;
+    bool manualForceZ = false;
+    bool manualAcceleration = false;
+    
     Server *server = nullptr;
     Client *client = nullptr;
-
+    
     QLineSeries *vibrationSeries = nullptr;
     QLineSeries *forceSeriesX = nullptr;
     QLineSeries *forceSeriesY = nullptr;
     QLineSeries *forceSeriesZ = nullptr;
     QLineSeries *accelerationSeries = nullptr;
     QSplineSeries *temperatureSeries = nullptr;
-
+    
     double vibration = 0, forceX = 0, forceY = 0, forceZ = 0, acceleration = 0;
     int currentTime = 0;
     double temperature = 20.0;
-
+    
     QDateTime lastVibWarningTime; // 마지막 진동 경고 발생 시각
     QDateTime lastTempWarningTime; // 마지막 온도 경고 발생 시각
-
+    
 };
 
 #endif // MAINWINDOW_H
